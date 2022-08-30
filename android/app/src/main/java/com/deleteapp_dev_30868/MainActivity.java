701a39f973206515f275f115ae22d13c8f9776cd
Crowdbotics <engineering@crowdbotics.com>
2022-08-30 09:58:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/deleteapp_dev_30868/MainActivity.java
@@ -0,0 +1,15 @@
+package com.deleteapp_dev_30868;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "deleteapp_dev_30868";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "deleteapp_dev_30868",
+  url: "https://deleteapp_dev_30868.botics.co",
+  api: "https://deleteapp_dev_30868.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
